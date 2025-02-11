07aa87f7c2e32dd421d25692cdfb6bdfbae3b73c
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-02-11 21:19:46 +0530

    added basic codegen

--- markleft-main.rb
+++ markleft-main.rb
@@ -93,6 +93,43 @@ class Parser
   end
 end
 
+class Generator
+  def generate(node)
+    case node
+    when BoldNode
+      generate_bold(node)
+    when ItalicsNode
+      generate_italics(node)
+    when NewlineNode
+      generate_newline(node)
+    when TextNode
+      generate_text(node)
+    else
+      raise RuntimeError.new("Unexpected node type: #{node.class}")
+    end
+  end
+
+  def generate_bold(node)
+    "<b>#{node.value}</b>"
+  end
+
+  def generate_italics(node)
+    "<i>#{node.value}</i>"
+  end
+
+  def generate_newline(node)
+    "<br>"
+  end
+
+  def generate_text(node)
+    node.value
+  end
+
+  def generate_all(nodes)
+    nodes.map { |node| generate(node) }.join
+  end
+end
+
 
 
 
@@ -102,7 +139,7 @@ end
 Token = Struct.new(:type, :value)
 BoldNode = Struct.new(:value)
 ItalicsNode = Struct.new(:value)
-NewlineNode = Struct.new
+NewlineNode = Struct.new(:value)
 TextNode = Struct.new(:value)
 
 # ----------------- Main Block ----------------- #
@@ -130,3 +167,6 @@ puts "Node Tree"
 node_tree.each do |node|
   puts node
 end
+
+generator = Generator.new.generate_all(node_tree)
+puts generator
--- sample.md
+++ sample.md
@@ -1,2 +1,2 @@
-This is a sample *Markdown* file
+This is a sample *Markdown* file \n
 **bold** yippee!
