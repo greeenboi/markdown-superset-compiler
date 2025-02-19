1d40b4eccadd8a84ee501b9abddb8451098c9a72
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-02-19 14:59:26 +0530

    adding unit tests

--- test/markleft_test.rb
+++ test/markleft_test.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative 'markleft_helper'
 
-class TestTest < Minitest::Markleft
+class MarkleftTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Markleft::VERSION
   end
